b71b6f9b262c15d359895ba784a00a901559dc9c
Miguelangel <silvermiguel96@gmail.com>
2019-03-05 14:13:37 -0500

    Promise

--- src/js/home.js
+++ src/js/home.js
@@ -5,13 +5,66 @@ console.log('Hola mundo!');
 // var método anterior para definir variables
 const noCambia = "Miguealangel";
 let cambia = "Rendon";
-
 // Funciones son piezas de codigo que podemos cambiar
-
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
-
  cambiaNombreFlecha =  (nuevoNombre) => {
   cambia = nuevoNombre
 }
+// const getUser = new Promise(function(todoBien, todoMal) {
+//   // LLamar a un api
+//   // Set
+//   setTimeout(function() {
+//     // Esperar 4 seggundo
+//     // todoBien();
+//     todoMal('Salido Todo Mal')
+//   }, 3000);
+// });
+// const getUserTime = new Promise(function(todoBien, todoMal) {
+//   // LLamar a un api
+//   // Set
+//   setTimeout( function() {
+//     // Esperar 4 seggundo
+//     // todoBien();
+//     todoMal('Todo esta mal')
+//   }, 5000);
+// });
+
+
+const getUser = new Promise(function(todoBien, todoMal) {
+  // LLamar a un api
+  // Set
+  setTimeout(function() {
+    // Esperar 4 seggundo
+    // todoBien();
+    todoBien('Bien promesa de 3')
+  }, 3000);
+});
+const getUserTime = new Promise(function(todoBien, todoMal) {
+  // LLamar a un api
+  // Set
+  setTimeout( function() {
+    // Esperar 4 seggundo
+    // todoBien();
+    todoBien('Bien promesa de 5')
+  }, 5000);
+});
+// getUser
+//   .then(function() {
+//     console.log('Todo esta bien en la promesa')
+//   })
+//   .catch(function() {
+//     console.log('Todo Mal')
+//   })
+// Promise.race Carrera de promesas
+Promise.all([
+  getUser,
+  getUserTime,
+])
+  .then(function(message) {
+    console.log(message);
+  })
+  .catch(function(message) {
+    console.log(message);
+  })
