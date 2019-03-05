5280a73a6f41b8f80230f282b7f4609ea0bfdc7d
Miguelangel <silvermiguel96@gmail.com>
2019-03-05 01:03:51 -0500

    variable and function

---
+++ src/js/home.js
@@ -0,0 +1,17 @@
+console.log('Hola mundo!');
+
+// const varibles que no cambian en el tiempo (constantes)
+// let variables que luego podemos cambiar
+// var método anterior para definir variables
+const noCambia = "Miguealangel";
+let cambia = "Rendon";
+
+// Funciones son piezas de codigo que podemos cambiar
+
+function cambiarNombre(nuevoNombre) {
+  cambia = nuevoNombre
+}
+
+ cambiaNombreFlecha =  (nuevoNombre) => {
+  cambia = nuevoNombre
+}
